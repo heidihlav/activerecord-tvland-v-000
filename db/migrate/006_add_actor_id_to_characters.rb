d481dd5a2e288f2014570dad59c2959a3682114a
Heidi Hlavinka <hchlav@gmail.com>
2018-11-28 01:44:17 +0000

    automatically backed up by learn

--- db/migrate/006_add_actor_id_to_characters.rb
+++ db/migrate/006_add_actor_id_to_characters.rb
@@ -1,8 +1,7 @@
 class AddActorIdToCharacters < ActiveRecord::Migration[4.2]
   def change
-    create_table :characters do |t|
-      t.string :name
-      t.integer :show_id
+    add_column :characters, :actor_id, :integer
+
     end
   end
 end
